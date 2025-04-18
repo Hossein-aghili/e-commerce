d760642e072d0beba224886eb3ccdcc2e911e260
Hossein <hossein.aghili.2658@gmail.com>
2025-04-18 21:40:54 +0330

    Creating a address model

---
+++ Models/addressMd.js
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const addressSchama = new mongoose.Schama({
+
+    city: {
+        type: String,
+        required: [true, 'city is required'],
+    },
+    state: {
+        type: String,
+        required: [true, 'state is required'],
+    },
+    street: {
+        type: String,
+        required: [true, 'street is required'],
+    },
+    description: {
+        type: String,
+        required: [true, 'description is required'],
+    },
+    receiverName: {
+        type: String,
+        required: [true, 'receiverName is required'],
+    },
+    postalCode: {
+        type: String,
+        required: [true, 'postalCode is required'],
+    },
+    userId: {
+        type: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+        default: []
+
+    },
+
+}, {
+    timestamps: true
+})
+
+export const Address = mongoose.model('Address', addressSchama)
+export default Address
