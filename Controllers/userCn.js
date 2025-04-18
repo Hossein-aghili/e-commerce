33a15b0953236f2b9a3bbdf3437aad789b7ecac6
Hossein <hossein.aghili.2658@gmail.com>
2025-04-18 22:13:13 +0330

    Creating a User Controllers

--- Controllers/userCn.js
+++ Controllers/userCn.js
@@ -1,14 +1,40 @@
+import User from "../Models/userMd.js";
 import catchAsync from "../Utils/catchAsync.js";
+import ApiFeatures from './../Utils/apiFeatures.js';
+import HandleERROR from './../Utils/handleError.js';
 
-
-export const getAll =catchAsync(async(req,res,next)=>{
-
+export const getAll = catchAsync(async (req, res, next) => {
+    const features = new ApiFeatures(User, req.query).filter().sort().limitFields().paginate()
+    const users = await features.query
+    const count = await User.countDocuments(req?.query?.filters)
+    return res.status(200).json({
+        status: 'success',
+        data: users,
+        count
+    })
 })
-export const getOne =catchAsync(async(req,res,next)=>{
-    
+export const getOne = catchAsync(async (req, res, next) => {
+    const { id } = req.params
+    if (id != req.userId && req.role != 'admin' && req.role != 'superAdmin') {
+        return next(new HandleERROR("you don't have a permission"))
+    }
+    const user = await User.findById(id)
+    return res.status(200).json({
+        status: 'success',
+        data: user
+    })
 })
 
-export const update =catchAsync(async(req,res,next)=>{
-    
+export const update = catchAsync(async (req, res, next) => {
+    const { id } = req.params
+    const { role = null, phoneNumber = null, boughtProductIds = null, ...others } = req.body
+    if (id != req.userId && req.role != 'admin' && req.role != 'superAdmin') {
+        return next(new HandleERROR("you don't have a permission"))
+    }
+    const user = await User.findByIdAndUpdate(id, others, { new: true })
+    return res.status(200).json({
+        status: 'success',
+        data: user
+    })
 })
 
