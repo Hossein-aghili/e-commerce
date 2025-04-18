1f4a54e10b7b46c16eac06595b9ef417a51d26ad
Hossein <hossein.aghili.2658@gmail.com>
2025-04-18 21:07:02 +0330

    Creating a User Model

---
+++ Models/userMd.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const userSchama = new mongoose.Schama({
+    firstName: String,
+    lastName: String,
+    birthDate: Date,
+    phoneNumber: {
+        type: String,
+        match: [/^(\+98|0)?9\d{9}$/, 'phone invalid'],
+        required: [true, 'phone number is required'],
+        unique: [true, 'phone number already taken']
+    },
+    favoriteProductIds: {
+        type: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'ProductId'
+        }],
+        default: []
+
+    },
+    password: {
+        type: String,
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user', 'superAdmin'],
+        default: 'user'
+    }
+}, {
+    timestamps: true
+})
+
+export const User = mongoose.model('User', userSchama)
+export default User
